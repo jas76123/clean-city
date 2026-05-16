1d463a30303b3e298514c36cbbc7937472a38f14
“Jasmin” <agababanz07@gmail.com>
2026-05-16 16:23:11 +0300

    feat(android/di): wire Koin in CleanCityApplication + OkHttp engine + EncryptedSharedPreferences

--- composeApp/src/androidMain/kotlin/com/example/cleancity/CleanCityApplication.kt
+++ composeApp/src/androidMain/kotlin/com/example/cleancity/CleanCityApplication.kt
@@ -1,7 +1,13 @@
 package com.example.cleancity
 
 import android.app.Application
+import com.example.cleancity.di.androidModule
+import com.example.cleancity.di.appModule
 import com.yandex.mapkit.MapKitFactory
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class CleanCityApplication : Application() {
     override fun onCreate() {
@@ -11,5 +17,10 @@ class CleanCityApplication : Application() {
         }
         MapKitFactory.setApiKey(BuildConfig.YANDEX_MAPS_API_KEY)
         MapKitFactory.initialize(this)
+        startKoin {
+            androidLogger(if (BuildConfig.IS_DEBUG) Level.ERROR else Level.NONE)
+            androidContext(this@CleanCityApplication)
+            modules(androidModule(), appModule())
+        }
     }
 }
---
+++ composeApp/src/androidMain/kotlin/com/example/cleancity/di/AndroidModule.kt
@@ -0,0 +1,15 @@
+package com.example.cleancity.di
+
+import com.example.cleancity.BuildConfig
+import com.example.cleancity.data.storage.TokenStorageFactory
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttp
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+fun androidModule(): Module = module {
+    single { NetworkConfig(baseUrl = BuildConfig.API_BASE_URL, isDebug = BuildConfig.IS_DEBUG) }
+    single { TokenStorageFactory(androidContext()) }
+    single<HttpClientEngine> { OkHttp.create() }
+}

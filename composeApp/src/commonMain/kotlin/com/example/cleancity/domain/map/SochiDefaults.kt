b1fd02e2ec6ff2d2b3fc505d136e97efb6703ae6
“Jasmin” <agababanz07@gmail.com>
2026-05-17 13:57:40 +0300

    feat(domain): CameraPosition, BoundingBox, SochiDefaults

---
+++ composeApp/src/commonMain/kotlin/com/example/cleancity/domain/map/SochiDefaults.kt
@@ -0,0 +1,6 @@
+package com.example.cleancity.domain.map
+
+object SochiDefaults {
+    val CENTER = CameraPosition(latitude = 43.5855, longitude = 39.7231, zoom = 12f)
+    val BBOX = BoundingBox(swLat = 43.40, swLon = 39.55, neLat = 43.75, neLon = 40.05)
+}
